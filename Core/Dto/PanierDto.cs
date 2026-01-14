a5918c07b471684d084cc9f345ba3d2c4201df5b
Téo <teomullerheddar@gmail.com>
2026-01-14 09:49:20 +0100

    refactor: remove PanierDto, PanierEntity, and PaniersRepository classes

--- Core/Dto/PanierDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Core.Dto;
-
-public class PanierDto
-{
-    public int Id { get; set; }
-    
-    public CustomerDto Customer { get; set; }
-    public IEnumerable<ProductDto>? Products { get; set; } = null;
-}
